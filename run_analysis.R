4e5eb6114a33a7ce9a33ddeefaa6e0b08cceb95c
AzAlejandro <alejo_367@hotmail.com>
2020-08-17 16:03:46 -0400

    new coments

--- run_analysis.R
+++ run_analysis.R
@@ -3,45 +3,49 @@ library(RCurl)
 
 
 #Read all the data and store in variables
-x_test <- read.table(sep = "", "../Getting-and-Cleaning-Data-Course-Project/getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/test/X_test.txt")
-x_train <- read.table(paste(sep = "", "../Getting-and-Cleaning-Data-Course-Project/getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/train/X_train.txt"))
-y_test <- read.table(paste(sep = "", "../Getting-and-Cleaning-Data-Course-Project/getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/test/y_test.txt"))
-y_train <- read.table(paste(sep = "", "../Getting-and-Cleaning-Data-Course-Project/getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/train/y_train.txt"))
-s_test <- read.table(paste(sep = "", "../Getting-and-Cleaning-Data-Course-Project/getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/test/subject_test.txt"))
-s_train <- read.table(paste(sep = "", "../Getting-and-Cleaning-Data-Course-Project/getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/train/subject_train.txt"))
-features <- read.table(paste(sep = "", "../Getting-and-Cleaning-Data-Course-Project/getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/features.txt"))
-activity_labels <- read.table(paste(sep = "", "../Getting-and-Cleaning-Data-Course-Project/getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/activity_labels.txt"))
+#Note script and extracted data should be in directory called "Getting-and-Cleaning-Data-Course-Project"
 
-#Tune some dataframes
+
+x_test <- read.table("../Getting-and-Cleaning-Data-Course-Project/UCI HAR Dataset/test/X_test.txt", sep = "")
+x_train <- read.table("../Getting-and-Cleaning-Data-Course-Project/UCI HAR Dataset/train/X_train.txt", sep = "")
+y_test <- read.table("../Getting-and-Cleaning-Data-Course-Project/UCI HAR Dataset/test/y_test.txt", sep = "")
+y_train <- read.table("../Getting-and-Cleaning-Data-Course-Project/UCI HAR Dataset/train/y_train.txt", sep = "")
+s_test <- read.table("../Getting-and-Cleaning-Data-Course-Project/UCI HAR Dataset/test/subject_test.txt", sep = "")
+s_train <- read.table("../Getting-and-Cleaning-Data-Course-Project/UCI HAR Dataset/train/subject_train.txt", sep = "")
+features <- read.table("../Getting-and-Cleaning-Data-Course-Project/UCI HAR Dataset/features.txt", sep = "")
+activity_labels <- read.table("../Getting-and-Cleaning-Data-Course-Project/UCI HAR Dataset/activity_labels.txt", sep = "")
+
+#Merge test and train data
+
+xdf <- rbind(x_test,x_train)
+ydf <- rbind(y_test,y_train)
+sdf <- rbind(s_test,s_train)
+
+#Get better features names and filter just mean and std names.
 
 filter_regex <- grep("mean+|std+", features$V2)
 sby_regex <- features[filter_regex, 2]
 sby_regex <- gsub("-mean", "Mean", sby_regex)
 sby_regex <- gsub("-std", "Std", sby_regex)
 sby_regex <- gsub("[-()]", "", sby_regex)
-#activity_labels$V2 <- as.character(activity_labels$V2)
-
-#Merge all data
-
-xdf <- rbind(x_test,x_train)
-ydf <- rbind(y_test,y_train)
-sdf <- rbind(s_test,s_train)
 
 
-#4. extract data by cols & using descriptive name
+#Filter data on values and merge with Users and Activity data
+
 xdf <- xdf[filter_regex]
 data_merged <- cbind(sdf, ydf, xdf)
 colnames(data_merged) <- c("User", "Activity", sby_regex)
 
-#data_merged$Activity <- factor(data_merged$Activity, levels = activity_labels[,1], labels = activity_labels[,2])
-#data_merged$User <- as.factor(data_merged$User)
 
-
-#5. generate tidy data set
+#Get tiddy dataframe
 gathered_data <- gather(data_merged, key, value, -User,-Activity)
-final_data <- gathered_data %>% group_by(User, Activity, key) %>%   summarise(media = mean(value)) %>% spread(key, media)
 
-#tidyData <- dcast(gathered_data, User + Activity ~ key, mean)
+#Get mean of values by Users, Activity, and feature
+final_data <- gathered_data %>% 
+  group_by(User, Activity, key) %>%   
+  summarise(media = mean(value)) %>% 
+  spread(key, media)
 
-23
+#Write data as txt to upload in git
+
 write.table(final_data, "./final_data.txt", row.names = FALSE)
